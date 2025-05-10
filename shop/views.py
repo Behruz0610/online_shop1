a9f7e55995de854dd0e0370dd49f0fae00e82600
Behruz Sattorov <bekhruzjan006@gmail.com>
2025-05-10 14:41:43 +0500

    yopildi

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,redirect
+from .models import Product,Category
 from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
+from .forms import OrderForm
+
 
 # Create your views here.
 
 
-def index(request):
-    products = Product.objects.all()
+def index(request,category_id=None):
+    search_query = request.GET.get('q','')
+    categories = Category.objects.all()
+    
+    
+    
+    
+    if category_id:
+        products = Product.objects.filter(category = category_id)
+    else:
+        products = Product.objects.all() #.order_by('-price')
+        
+    if search_query:
+        products = products.filter(name__icontains = search_query)
+    
+    
     context = {
         'products': products,
+        'categories': categories
     }
     return render(request, 'shop/home.html',context)
 
@@ -23,6 +41,21 @@ def product_detail(request,product_id):
         return HttpResponse('Product Not Found')
     
     
-    
-    
-    
+def order_detail(request,pk):
+    # product = Product.objects.get(id = pk)
+    product = get_object_or_404(Product,pk=pk)
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.product = product
+            order.save()
+            return redirect('index')
+    context = {
+        'form':form,
+        'product':product
+    }
+            
+    return render(request,'shop/detail.html',context = context)
+            
